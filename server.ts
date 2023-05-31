c3cbac20c796bd27d237427ff5ccf57b62d7227e
Vadim <vadim.zinchenco@mail.ru>
2023-05-31 18:16:24 +0200

    feat: update express server

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}!`);
