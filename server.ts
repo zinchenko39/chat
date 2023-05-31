5b02735c11b7953a763e1efb88bb8adb05b10bb6
Vadim <vadim.zinchenco@mail.ru>
2023-05-31 18:59:08 +0200

    feat: send to second review

--- server.ts
+++ server.ts
@@ -1,11 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(PORT, function () {
-  console.log(`App listening on port ${PORT}!`);
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is starting on port ${PORT}!`);
 });
