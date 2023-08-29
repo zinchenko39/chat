a47f221335430c97f7db6ae31264529d5a697736
Vadim <vadim.zinchenco@mail.ru>
2023-08-29 11:29:22 +0200

    fix: transfer to webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    historyApiFallback: true, // Эта опция перенаправит все неизвестные запросы на index.html.
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.pug$/,
+        use: ['pug-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
