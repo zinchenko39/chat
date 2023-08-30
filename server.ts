52f40ec66e58f894e76bafc3a1aecce41180c31a
Vadim <vadim.zinchenco@mail.ru>
2023-08-30 14:23:42 +0200

    fix: add webpack plugin

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.listen(PORT, () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './index.ts',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '/index.html',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
